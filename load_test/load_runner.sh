83883187a2582139d6f8f33fdd08081fe3ed8ece
Basar Sohail Chowdhury <sohail.chd0202@gmail.com>
2018-12-30 12:59:48 -0500

    dockerfile for load_test

--- load_test/load_runner.sh
+++ load_test/load_runner.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+echo "Locust load test started...."
+
+
+echo "Setting root dir"
 root_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo "root_dir : "$root_dir" "
 
 export PYTHONPATH=$PYTHONPATH:$root_dir:$root_dir/common:$root_dir/locustfiles:$root_dir/reports
 
+echo "starting locust..."
 locust -f  $root_dir/locustfiles/Locust_footBallOrg.py --no-web -c 1000 -r 67 --run-time 15sec --print-stats --only-summary \
 --csv=$root_dir/reports/locust_report  --logfile=$root_dir/reports/locustfile.log
